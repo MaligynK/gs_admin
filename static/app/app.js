613b378e068468d2168fc217897cea11039e5453
MaligynK <skull_mk@mail.ru>
2014-10-29 19:22:55 +0300

    0.2

--- static/app/app.js
+++ static/app/app.js
@@ -1,4 +1,4 @@
-app = angular.module('admin', ['ui.bootstrap', 'ngRoute', 'ngResource', 'ui.router']);
+app = angular.module('admin', ['ui.bootstrap', 'ngRoute', 'ngResource', 'ui.router', 'ngTouch']);
 
 app.config(function($httpProvider){
     $httpProvider.defaults.xsrfCookieName = "csrftoken";
@@ -20,6 +20,28 @@ app.controller('CommonController', ['$scope', 'dataService',
         $scope.data = {};
         $scope.data.server = 5001;
         dataService.data = $scope.data;
+
+        dataService.status = {
+                        0:  'Ход белых',
+                        1:  'Ход черных',
+                        11: 'Победа белых',
+                        12: 'Победа белых - время истекло',
+                        13: 'Победа белых - оппонент сдался',
+                        21: 'Победа черных',
+                        22: 'Победа черных - время истекло',
+                        23: 'Победа черных - оппонент сдался',
+                        30: 'Ничья',
+                        31: 'Пат',
+                        32: 'Правило 50 ходов',
+                        33: 'Правило трех повторений',
+                        34: 'Недостаточно фигур, чтобы поставить мат',
+                        40: 'Белые отменили игру',
+                        41: 'Черные отменили игру',
+                        42: 'Игра отменена - время истекло',
+                        50: 'Ожидание противника'
+        };
+
+
 	}
 ]);
 
@@ -33,29 +55,23 @@ app.config(function($stateProvider, $routeProvider){
             views: {
                 '': {
                     templateUrl: "static/app/templates/current_games.html",
-                    controller: ['$scope', '$http', '$rootScope', 'dataService',
-                        function ($scope, $http, $rootScope, dataService) {
-
-                            $http.jsonp('http://gs' + dataService.data.server + '.1chess.org/gs_admin/games.get/?callback=JSON_CALLBACK').success(function (data) {
-                                $scope.current_games = data.items;
-                                if($scope.current_games.length > 100){
-                                    $scope.games = $scope.current_games.slice(0,100);
-                                } else {
-                                    $scope.games = $scope.current_games;
-                                }
-                                $scope.page = 1;
+                    controller: ['$scope', '$http', 'dataService', '$log',
+                        function ($scope, $http, dataService, $log) {
+                            $scope.status = dataService.status;
+                            $scope.server = dataService.data.server;
+                            $http.jsonp('http://gs' + $scope.server + '.1chess.org/gs_admin/games.get/?callback=JSON_CALLBACK').success(function (data) {
+                                $scope.games = data.items;
+                                $scope.page = 0;
                             }).error(function (err) {
-                                console.log('error',err);
+                                $log.info('error',err);
                             });
 
                             $scope.getPage = function (page){
                                 page = $scope.page + page;
-                                if (page > 0 && $scope.current_games.length > (page * 100 - 100)){
-                                    $scope.games = $scope.current_games.slice(page * 100 - 101, page * 100);
+                                if (page >= 0 && $scope.games.length > (page * 100)){
                                     $scope.page = page;
                                 }
                             }
-
 
                         }
                     ]
@@ -66,17 +82,56 @@ app.config(function($stateProvider, $routeProvider){
             url: '/archive_games',
 			views: {
                 '': {
-                    templateUrl: "static/app/templates/current_games.html",
-                    controller: ['$scope', '$http', '$q',
-                        function($scope, $http, $q) {
+                    templateUrl: "static/app/templates/archive_games.html",
+                    controller: ['$scope', '$http', '$q', 'dataService', '$log',
+                        function($scope, $http, $q, dataService, $log) {
 
-						
+                            $scope.status = dataService.status;
+
+                            $http.jsonp('http://gs' + dataService.data.server + '.1chess.org/gs_admin/archive_games.get/?callback=JSON_CALLBACK').success(function (data) {
+                                $scope.games = data.items;
+                                $scope.page = 0;
+                            }).error(function (err) {
+                                $log.info('error',err);
+                            });
+
+                            $scope.getPage = function (page){
+                                page = $scope.page + page;
+                                if (page >= 0 && $scope.games.length > (page * 100)){
+                                    $scope.page = page;
+                                }
+                            }
 							
                         }
                     ]
                 }
             }
         })
+
+        .state('last_turns', {
+            url: '/last_turns',
+			views: {
+                '': {
+                    templateUrl: "static/app/templates/last_turns.html",
+                    controller: ['$scope', '$http', '$q', 'dataService', '$log',
+                        function($scope, $http, $q, dataService, $log) {
+                            $scope.server = dataService.data.server;
+                            $scope.time = new Date();
+                            $http.jsonp('http://gs' + $scope.server + '.1chess.org/gs_admin/turns.get/?callback=JSON_CALLBACK').success(function (data) {
+                                $scope.turns = data.items;
+                            }).error(function (err) {
+                                $log.info('error',err);
+                            });
+                            $scope.searchID = function(id){
+                                $scope.search = id;
+                            }
+                        }
+                    ]
+                }
+            }
+        })
+
+
         .state('users', {
             url: '/users',
             views: {
@@ -92,10 +147,6 @@ app.config(function($stateProvider, $routeProvider){
                 }
             }
         })
-
-
-
-
 
 });
 
